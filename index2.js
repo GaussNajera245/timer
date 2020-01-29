45c86f1082ee79d0fbe6ad0ce1126a9870a9beff
GusNajera <gvt_a9@hotmail.com>
2020-01-28 18:05:59 -0600

    adding the other button functionality

--- index2.js
+++ index2.js
@@ -1,8 +1,4 @@
-
-// new Vue({
-//     el: '#app',
-//     vuetify: new Vuetify(),
-//   })
+var x;
 
 var newest = new Vue({
     el: '#nemo',
@@ -12,31 +8,30 @@ var newest = new Vue({
     },
     methods: {
         start: function () {
-            let self = this;
-            let some = self.message.split(':');
-            let min1 = parseInt(some[0]);
-            let seg1 = parseInt(some[1]);
-
-            var check = setInterval(function () {
-                seg1 += 1;
-                if (seg1 == 60) {
-                    min1 += 1;
-                    seg1 = 0;
-                }
-
-                let seg = seg1.toString().padStart(2, '0');
-                let min = min1.toString().padStart(2, '0');
-
-                self.message = [min, seg].join(':');
-
-            }, 1000);
-
-            return check;
+            check();
         },
         stop: function () {
-            clearInterval(this.start());
-
-            
+            clearInterval(x);
         }
     }
 }); 
+
+let some = newest.message.split(':');
+let min1 = parseInt(some[0]);
+let seg1 = parseInt(some[1]);
+
+function check(){
+    x = setInterval(function () {
+        seg1 += 1;
+        if (seg1 == 60) {
+            min1 += 1;
+            seg1 = 0;
+        }
+
+        let seg = seg1.toString().padStart(2, '0');
+        let min = min1.toString().padStart(2, '0');
+
+        newest.message = [min, seg].join(':');
+
+    }, 1000);      
+}
